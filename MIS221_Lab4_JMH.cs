0d3993be8e8868a463db21684c01f555ec4ef5a0
JMHibbard <jmhibbard@crimson.ua.edu>
2024-09-30 11:58:49 -0500

    Add files via upload

---
+++ MIS221_Lab4_JMH.cs
@@ -0,0 +1,77 @@
+using System;
+
+int whatEva = GetANumba();
+
+Random random = new Random();
+int randomRows = random.Next(8,12);
+
+while(whatEva != 4){
+    if(whatEva == 1){
+        NothinGabagool();
+    } else if (whatEva == 2){
+        CheesyGabagool();
+    } else if (whatEva == 3){
+        PiggyGabagool();
+    }
+    //} catch(Exception e){
+        //System.Console.WriteLine(e.Message);
+    //}
+    whatEva = GetANumba();
+}
+
+
+/*for(int i = 0; i < random; i++){
+    for(int j = 1; j < random - i; j++){
+        Console.Write("* ");
+    }
+    System.Console.WriteLine();
+}*/
+
+//j1: Boneless Pizza??? What is wrong with you?
+static void NothinGabagool(){
+    for(int i = 0; i < random; i++){
+        for(int j = 1; j < random - i; j++){
+            System.Console.Write("* ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+//j2: The old classic? Nothin wrong with that, just make sure you can handle your dairy.
+static void CheesyGabagool(){
+    for(int i = 0; i < random; i++){
+        for(int j = 0; j < random - i; j++){
+            if(j == 0 || j == random-i-1){
+                System.Console.Write("* ");
+            } else if(i != 0){
+                System.Console.Write("# ");
+            }        
+        }
+        System.Console.WriteLine();
+    }
+}
+
+//j3: Pepperoni? Calazoni? What's the difference, the piggy be oinkin.
+static void PiggyGabagool(){
+    for(int i = 0; i < random; i++){
+        for(int j = 1; j < random - i; j++){
+            //Assigning new random variable for pepperoni slices
+            Random pepRand = new Random();
+            int pepRand = pepRand.Next(1,random - 1);
+            //
+            if(j == 0 || j == random-i-1){
+                System.Console.Write("* ");
+            } else if(j != 0){
+                System.Console.Write("# ");
+            } else if(j != 0 && j == pepSpot){
+                System.Console.Write("[] ");
+            }
+        }
+        System.Console.WriteLine();
+    }
+}
+
+static int GetANumba(){
+    System.Console.WriteLine("What ya want? 1, 2, or 3? If you say 4, no pizza for you.");
+    return int.Parse(Console.ReadLine());
+}
